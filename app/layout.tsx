3ee9412882c90128cc8e1072aa8b4459df7ca236
RebeccaMeegahapola <rebeccameegahapola@gmail.com>
2026-05-04 19:05:04 +0530

    feat: add Navbar and Footer components to layout for improved structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import {Geist, Geist_Mono, Inter, Poppins} from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
 
 const poppins = Poppins({
   variable: "--font-heading",
@@ -29,7 +31,15 @@ export default function RootLayout({
       lang="en"
       className={`${poppins.variable} ${inter.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+      <Navbar />
+
+      <main className="flex-1">
+        {children}
+      </main>
+
+      <Footer />
+      </body>
     </html>
   );
 }
