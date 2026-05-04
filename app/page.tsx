9f36513c9766dc74135fea19468332a5f56feef1
RebeccaMeegahapola <rebeccameegahapola@gmail.com>
2026-05-04 19:05:24 +0530

    chore: remove Navbar component from the Home function in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,12 @@
-import Navbar from "@/components/layout/Navbar";
 import Hero from "@/components/sections/Home/Hero";
 import About from "@/components/sections/Home/About";
 import Services from "@/components/sections/Home/Services";
 import OurProcess from "@/components/sections/Home/OurProcess";
 import CTASection from "@/components/sections/Home/CTASection";
 
-
 export default function Home() {
     return (
         <main className="min-h-screen">
-            <Navbar />
             <Hero />
             <Services />
             <About />
