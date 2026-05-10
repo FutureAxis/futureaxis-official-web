ea7fb199f83869e4878c2d179916900ba27c679a
RebeccaMeegahapola <rebeccameegahapola@gmail.com>
2026-05-10 13:11:23 +0530

    chore: update import path for OurProcess and add ValuesSection to About page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,12 +1,14 @@
-import OurProcess from "@/components/sections/Home/OurProcess";
+import OurProcess from "@/components/sections/About/OurProcess";
 import Hero from "@/components/sections/About/Hero";
 import AboutSection from "@/components/sections/About/AboutSection";
+import ValuesSection from "@/components/sections/About/ValuesSection";
 
 export default function Home() {
     return (
         <main className="min-h-screen">
             <Hero />
             <AboutSection />
+            <ValuesSection />
             <OurProcess />
         </main>
     );
