1c3d248a4d67a9162d212a38a85c8057cdcad5dd
RebeccaMeegahapola <rebeccameegahapola@gmail.com>
2026-05-12 16:41:14 +0530

    feat: add Home component with ServicesHero to the page service

---
+++ app/services/page.tsx
@@ -0,0 +1,9 @@
+import ServicesHero from "@/components/services/Hero";
+
+export default function Home() {
+    return (
+        <main className="min-h-screen">
+            <ServicesHero />
+        </main>
+    );
+}
