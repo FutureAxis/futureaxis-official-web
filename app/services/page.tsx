37e53a1d186ae95a376045b0c2f5f77d8c18ecbb
RebeccaMeegahapola <rebeccameegahapola@gmail.com>
2026-05-25 09:43:02 +0530

    chore: remove unwanted files

--- app/services/page.tsx
+++
@@ -1,9 +0,0 @@
-import ServicesHero from "@/components/services/Hero";
-
-export default function Home() {
-    return (
-        <main className="min-h-screen">
-            <ServicesHero />
-        </main>
-    );
-}
