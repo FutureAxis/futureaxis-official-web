ae4066b0a6948aaadf55d1a8d2ae3c16f270aaac
RebeccaMeegahapola <rebeccameegahapola@gmail.com>
2026-05-25 13:52:36 +0530

    refactor: update Home component to include background image and updated sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,35 @@
-import Hero from "@/components/sections/Home/Hero";
-import About from "@/components/sections/Home/About";
-import Services from "@/components/sections/Home/Services";
-import OurProcess from "@/components/sections/Home/OurProcess";
-import CTASection from "@/components/layout/CTASection";
+"use client";
+
+import Image from "next/image";
+import HeroSection from "@/components/sections/Home/Hero";
+import AboutSection from "@/components/sections/Home/About";
+import backgroundImage from "@/public/images/home/background-image.png";
+import ProcessSection from "@/components/sections/Home/OurProcess";
+import ServicesSection from "@/components/sections/Home/Services";
 
 export default function Home() {
     return (
-        <main className="min-h-screen">
-            <Hero />
-            <Services />
-            <About />
-            <OurProcess />
+        <main className="relative min-h-screen">
+            <HeroSection />
+
+            <div className="relative">
+                <div className="absolute inset-0 z-0">
+                    <Image
+                        src={backgroundImage}
+                        alt="Background"
+                        fill
+                        className="object-cover object-center"
+                        quality={100}
+                        priority
+                        sizes="100vw"
+                    />
+                </div>
+                <div className="relative z-10">
+                    <AboutSection />
+                    <ServicesSection />
+                    <ProcessSection />
+                </div>
+            </div>
         </main>
     );
 }
