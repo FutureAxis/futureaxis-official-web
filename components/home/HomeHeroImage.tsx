30afa4897624e60b423459641ae2c400aca486b8
RebeccaMeegahapola <rebeccameegahapola@gmail.com>
2026-06-10 10:04:45 +0530

    chore: remove unwanted files

--- components/home/HomeHeroImage.tsx
+++
@@ -1,9 +0,0 @@
-import DashboardMockup from "@/components/ui/DashboardMockup";
-
-export default function HomeHeroImage() {
-    return (
-        <div className="w-full lg:w-[52%] h-[360px] sm:h-[400px] lg:h-[460px] xl:h-[520px]">
-            <DashboardMockup />
-        </div>
-    );
-}
